d4d7a92beee561461f2222165d7609bde21bdaa2
AxelFerguson <125918403+AxelFerguson@users.noreply.github.com>
2024-09-09 11:10:46 -0700

    Update add_stock_flow_trigger.sql
    fixed upload format errors

--- add_stock_flow_trigger.sql
+++ add_stock_flow_trigger.sql
@@ -3,38 +3,34 @@
  CREATE TRIGGER add_stock_flow
   AFTER INSERT 
      ON blockchain
-/*
-   WHEN  (SELECT MAX(date)
-          FROM stockflow) < (SELECT DATE(mediantime, 'unixepoch', 'localtime')
-		                       FROM blockchain)
-*/
+
 BEGIN
  
  INSERT INTO stockflow (date, flow_365_p, total_stock, stock_flow, supply_growth_rate)
  
  SELECT f.date, f.flow AS flow_365_p,
-              MAX(c.total_supply) AS total_stock,
-			  MAX(c.total_supply)/f.flow AS stock_flow,
-			  (f.flow/MAX(c.total_supply)) AS supply_growth_rate
-  FROM (SELECT DATE(mediantime, 'unixepoch', 'localtime') AS date, SUM(SUM((subsidy/100000000.0))) OVER (
-			                        ORDER by DATE(mediantime, 'unixepoch', 'localtime')
-						             ROWS BETWEEN 365 PRECEDING
-						              AND CURRENT ROW) AS flow
-          FROM blockchain
-         GROUP BY DATE(mediantime, 'unixepoch', 'localtime')
+        MAX(c.total_supply) AS total_stock,
+	MAX(c.total_supply)/f.flow AS stock_flow,
+	(f.flow/MAX(c.total_supply)) AS supply_growth_rate
+   FROM (SELECT DATE(mediantime, 'unixepoch', 'localtime') AS date, SUM(SUM((subsidy/100000000.0))) OVER (
+	  ORDER by DATE(mediantime, 'unixepoch', 'localtime')
+	   ROWS BETWEEN 365 PRECEDING
+	    AND CURRENT ROW) AS flow
+           FROM blockchain
+          GROUP BY DATE(mediantime, 'unixepoch', 'localtime')
 		 ) AS f
-  JOIN (SELECT DATE(mediantime, 'unixepoch', 'localtime') AS date,
-        SUM((subsidy/100000000.0)) OVER(
-                   ORDER BY height
-	                ROWS BETWEEN UNBOUNDED PRECEDING
-	                 AND CURRENT ROW) AS total_supply
-         FROM blockchain
-        ORDER BY height
+   JOIN (SELECT DATE(mediantime, 'unixepoch', 'localtime') AS date,
+                SUM((subsidy/100000000.0)) OVER(
+          ORDER BY height
+	   ROWS BETWEEN UNBOUNDED PRECEDING
+	    AND CURRENT ROW) AS total_supply
+           FROM blockchain
+          ORDER BY height
 		) AS c
-    ON f.date = c.date
- GROUP BY c.date
+     ON f.date = c.date
+  GROUP BY c.date
  HAVING (f.date NOT IN (SELECT date
                         FROM stockflow))
     AND (f.date NOT IN (SELECT DATE('now')))
-	AND ((SELECT MAX(DATE(mediantime, 'unixepoch', 'localtime')) FROM blockchain) IN (SELECT DATE('now')));
+    AND ((SELECT MAX(DATE(mediantime, 'unixepoch', 'localtime')) FROM blockchain) IN (SELECT DATE('now')));
 END;
