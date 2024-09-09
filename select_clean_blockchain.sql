594ed455b6179d9bdde24e7876c05dae513a7d4a
AxelFerguson <125918403+AxelFerguson@users.noreply.github.com>
2024-09-09 11:12:58 -0700

    Update select_clean_blockchain.sql
    Corrected upload format errors

--- select_clean_blockchain.sql
+++ select_clean_blockchain.sql
@@ -22,9 +22,9 @@ d_reward AS
 flow_365_pre AS
 (
 SELECT date, SUM(daily_reward) OVER (
-			                        ORDER by date
-						             ROWS BETWEEN 365 PRECEDING
-						              AND CURRENT ROW
+                                     ORDER by date
+                                      ROWS BETWEEN 365 PRECEDING
+				       AND CURRENT ROW
 				) AS flow
   FROM d_reward
  GROUP BY date  
@@ -34,10 +34,10 @@ cumulative_r AS
 (
  SELECT date, reward, height,
         SUM(reward) OVER(
-                   ORDER BY height
-	                ROWS BETWEEN UNBOUNDED PRECEDING
-	                 AND CURRENT ROW
-	   ) AS total_supply
+                         ORDER BY height
+	                  ROWS BETWEEN UNBOUNDED PRECEDING
+	                   AND CURRENT ROW
+	            ) AS total_supply
   FROM time_subsidy
  ORDER BY height
 ),
@@ -45,9 +45,9 @@ cumulative_r AS
 stock_flow AS
 (
  SELECT f.date, f.flow AS flow_365_p,
-              MAX(c.total_supply) AS total_stock,
-			  MAX(c.total_supply)/f.flow AS stock_flow,
-			  (f.flow/MAX(c.total_supply)) AS supply_growth_rate
+        MAX(c.total_supply) AS total_stock,
+	MAX(c.total_supply)/f.flow AS stock_flow,
+	(f.flow/MAX(c.total_supply)) AS supply_growth_rate
   FROM flow_365_pre f
   JOIN cumulative_r c
     ON f.date = c.date
