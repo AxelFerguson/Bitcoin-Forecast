a4d7ec24448db8977adafa7727e67f8b515d7691
AxelFerguson <125918403+AxelFerguson@users.noreply.github.com>
2024-09-09 22:02:41 -0700

    Update create_view_bitcoinpricesf.sql
    Improved comments

--- create_view_bitcoinpricesf.sql
+++ create_view_bitcoinpricesf.sql
@@ -1,5 +1,8 @@
 DROP VIEW IF EXISTS bitc_price_sf;
 
+/*
+This view selects the relevant data for the machine learning model. The date, price low and high, the stock flow, and stock growth rate.
+*/
 CREATE VIEW bitc_price_sf
 AS
 
